fe768eaa743488d69cd4228481a635ece41ce8f6
Kenluckyz <aoken0991242405@gmail.com>
2026-09-22 13:46:40 +0700

    wip

--- Assets/Midterm Exam/Prob01-ClassDiagram/GameEntity.cs
+++ Assets/Midterm Exam/Prob01-ClassDiagram/GameEntity.cs
@@ -1,9 +1,11 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace MidtermExam.Prob01
 {
     public class GameEntity
     {
+
         // TODO: Implement fields and methods according to Class Diagram
     }
 }
